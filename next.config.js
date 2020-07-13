48838ba1e814d3cf8c9376f59fbcce9b9262ed2a
Matt <carlotta.matt@gmail.com>
2020-07-13 16:11:23 -0700

    Update docs

--- next.config.js
+++ next.config.js
@@ -2,16 +2,19 @@ const openBrowser = require("react-dev-utils/openBrowser");
 const plugins = require("./config/plugins");
 
 const { NODE_ENV, LOCALHOST } = process.env;
+const inProd = NODE_ENV === "production";
+const assetPrefix = inProd
+  ? "https://mattcarlotta.github.io/composable-styled-components/"
+  : "";
 
 /* opens a browser window */
 if (NODE_ENV === "development") openBrowser(`${LOCALHOST}`);
 
 module.exports = {
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" }
-    };
-  },
+  assetPrefix,
+  exportPathMap: () => ({
+    "/": { page: "/" }
+  }),
   webpack(config, { isServer }) {
     /* adds custom plugins to client and server */
     config.plugins.push(...plugins(isServer));
