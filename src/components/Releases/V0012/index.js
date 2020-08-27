86eceb1fa3dd9b86d4fabf6bede32a1c5bdad785
Matt <carlotta.matt@gmail.com>
2020-08-26 18:41:14 -0700

    Fix doc spelling error

--- src/components/Releases/V0012/index.js
+++ src/components/Releases/V0012/index.js
@@ -7,7 +7,7 @@ const V0012 = () => (
       v0.0.12
     </AnchorTitle>
     <ReleaseNotes>
-      <li>Updated README to imgshield tags</li>
+      <li>Updated README to include imgshield tags</li>
       <li>Updated README to include package exports documentation link</li>
       <li>Updated documentation website to include package exports</li>
       <li>Added e2e tests to include package exports</li>
